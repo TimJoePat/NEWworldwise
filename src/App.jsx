8a4a84f9d38c27d4fd1289ee17c730763f2758a4
TimJoePat <TimJoePat88@cox.net>
2024-02-06 09:00:25 -0700

    212-Nested Routes and Index Route complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Login from "./pages/Login";
@@ -9,11 +13,26 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route
+            index
+            element={<p>List of cities</p>}
+          />
+          <Route
+            path="cities"
+            element={<p>List of cities</p>}
+          />
+
+          <Route
+            path="countries"
+            element={<p>List of countries</p>}
+          />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
